cdc89b15c97d5c55d409635369c7e4c0421e2afb
sugarware <46047287+sugarware@users.noreply.github.com>
2026-09-09 15:13:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE="id-portal-v0.7.2";
+const CACHE="id-portal-v0.8";
 const APP=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png",
-"https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js",
+"https://cdnjs.cloudflare.com/ajax/libs/qrcode-generator/1.4.4/qrcode.min.js",
+"https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.js",
 "https://cdn.jsdelivr.net/npm/jsbarcode@3.11.6/dist/JsBarcode.all.min.js",
 "https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(async c=>{for(const u of APP){try{await c.add(u)}catch{}}}));self.skipWaiting();});
